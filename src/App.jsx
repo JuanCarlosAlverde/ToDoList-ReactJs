a8cf0af689baac645bf099dce18ab65ee7156730
Juan Carlos Alverde <alverdejuancarlos@gmail.com>
2020-11-26 23:57:24 -0600

    1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,16 @@
-import React, { Component } from "react";
-import Title from "./components/Title";
-import {Switch , Route} from 'react-router-dom'
-import SearchForm from "./components/SearchForm";
-import "./App.css";
-import MoviesList from "./components/MoviesList";
-import "bulma/css/bulma.css";
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      results: [],
-      usedSearch: false
-    };
-  }
-
-  _handleResults = (result) => {
-    this.setState({ results: result, usedSearch:true });
-  };
-
-  _renderResults=()=>{
-    return this.state.results.length === 0 ? (
-      <p>Sorry results not found</p>
-    ) : (
-      <MoviesList results={this.state.results} />
-    )
-  }
-
-  render() {
-    const { results,usedSearch } = this.state;
+import React from 'react'
+import MemeGenerator from './MemeGenerator'
+import Header from './Header'
 
 
-    return (
-      <div className="App">
-        <Title>Search Movie</Title>
-        <div className="s-w">
-          <SearchForm onResults={this._handleResults} />
+function App(){
+    return(
+        <div>
+            <Header/>
+            <MemeGenerator/>
+            
         </div>
-
-        {usedSearch ? 
-          this._renderResults()
-         : 
-          <small>Use the form to serch a movie</small>
-        }
-      </div>
-    );
-  }
+    )
 }
 
 export default App;
