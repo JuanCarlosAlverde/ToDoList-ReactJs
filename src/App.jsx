715748e319495a548aa3583182ef92f7c1064ea3
Juan Carlos Alverde <alverdejuancarlos@gmail.com>
2020-11-27 00:23:28 -0600

    1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import MemeGenerator from './MemeGenerator'
-import Header from './Header'
+import React from "react";
+import Info from "./info";
+import data from "./data.json";
 
-
-function App(){
-    return(
-        <div>
-            <Header/>
-            <MemeGenerator/>
-            
-        </div>
-    )
+function App() {
+    const arr1= data.map(elem=><Info key={elem.id} question={elem.punchLine} answer={elem.question}/>)
+  return (
+    <div>
+      <h3>Joke-o-Meter</h3>
+      {arr1}
+    </div>
+  );
 }
 
 export default App;
